c7a6c9b5d367c1911f8485ef911068405979b1df
Xinchen Liu <lxc86739795@gmail.com>
2021-05-18 09:58:22 +0800

    Delete data/.ipynb_checkpoints directory

--- data/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-"""
-@author:  sherlock
-@contact: sherlockliao01@gmail.com
-"""
-
-# Changed by Xinchen Liu
-
-from .build import get_dataloader, get_test_dataloader, get_ins_dataloader, get_dataloader_mask, get_test_dataloader_mask
